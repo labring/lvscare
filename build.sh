d5708cb8b4ddf93de1aa2e9461526d405f80e5af
cuisongliu <cuisongliu@qq.com>
2020-04-15 18:04:05 +0800

    fix version

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 # build.sh v3.0.2
+set -x
 COMMIT_SHA1=$(git rev-parse --short HEAD || echo "0.0.0")
 go build -o lvscare -mod vendor -ldflags "-X github.com/fanux/lvscare/version.Version=$1 -X github.com/fanux/lvscare/version.Build=${COMMIT_SHA1}" main.go
